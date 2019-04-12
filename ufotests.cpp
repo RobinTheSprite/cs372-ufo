e01356e4ab8f41c300b7cd0fd51155a65dec805a
ngsimeon <ngsimeon90@gmail.com>
2019-04-12 14:01:27 -0800

    Add testing file

---
+++ ufotests.cpp
@@ -0,0 +1,13 @@
+// ufotests.cpp
+// For catch testing
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// TODO:
+// Test retrieval of a vector of WIN32_FIND_DATA
+// - creation time
+// - last access time
+// - file size
+// - file name
+//
