c4c10af8dbf34e0aeddd2d33409cfb9e3e410b12
Mark Underwood <mtunderwood@alaska.edu>
2019-04-11 18:19:59 -0800

    Add header file for windows file retriever

---
+++ retriever-win.h
@@ -0,0 +1,21 @@
+// retriever-win.cpp.h
+// Created by Mark on 4/9/2019.
+//
+
+#ifndef CS372_UFO_RETRIEVER_WIN_CPP_H
+#define CS372_UFO_RETRIEVER_WIN_H
+
+#include <Windows.h>
+#include <vector>
+#include "file-folder.h"
+
+namespace ufo
+{
+
+std::vector<ufo::file> retrieveWin();
+
+}
+
+
+
+#endif //CS372_UFO_RETRIEVER_WIN_CPP_H
