e28dcb0058e6a3ec81d9aade658f62f636e320b9
round-thing-over-by-that-square <lewandowski.alex@gmail.com>
2019-04-15 10:05:22 -0800

    test accessing file's data members. Crashes.

--- ufotests.cpp
+++ ufotests.cpp
@@ -4,6 +4,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "ufo.h"
+#include <iostream>
 
 // TODO:
 // Test retrieval of a vector of WIN32_FIND_DATA
@@ -18,11 +19,15 @@ TEST_CASE("File Retrieval: Empty Path") {
     auto retrievedRootFolder = organizer.retrieve();
 }
 
-
+// TODO: crashes when accessing a retrieved file's data members: "Process finished with exit code -1073741819 (0xC0000005)"
 TEST_CASE("File Retrieval: with path to file")
 {
-    ufo::Ufo organizer("C:\\Users\\AlexL\\Documents\\UAF Classes\\Software Construction\\cs372-ufo\\our_directory_for_testing\\license.txt");
-    auto retrievedFile = organizer.retrieve();
+    ufo::Ufo organizer("C:\\Users\\AlexL\\Documents\\UAF Classes\\Software Construction\\cs372-ufo\\our_directory_for_testing");
+    auto retrievedFolder = organizer.retrieve();
+   // auto path = retrievedFolder[0].path;
+   // auto name = retrievedFolder[0].name;
+   // auto size = retrievedFolder[0].size;
+   // auto dateCreated = retrievedFolder[0].dateCreated;
+   // auto dateModified = retrievedFolder[0].dateModified;
 
 }
-
