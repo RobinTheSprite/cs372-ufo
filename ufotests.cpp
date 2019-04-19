a099d8059aabe7df1421c2372c26eb9c72788761
Mark Underwood <mtunderwood@alaska.edu>
2019-04-18 20:53:54 -0800

    Relative file paths usable

--- ufotests.cpp
+++ ufotests.cpp
@@ -15,21 +15,23 @@
 //
 
 TEST_CASE("File Retrieval: Empty Path") {
-    ufo::Ufo organizer("");
+    ufo::Ufo organizer(".");
     auto retrievedRootFolder = organizer.retrieve();
+
+    REQUIRE(retrievedRootFolder[0].name == "CMakeCache.txt");
+    REQUIRE(retrievedRootFolder[0].path.rfind(R"(\cmake-build-debug\CMakeCache.txt)") != string::npos);
 }
 
 // TODO: crashes when accessing a retrieved file's data members: "Process finished with exit code -1073741819 (0xC0000005)"
 TEST_CASE("File Retrieval: with path to file")
 {
     ufo::Ufo organizer(
-            R"(C:\Users\AlexL\Documents\UAF Classes\Software Construction\cs372-ufo\our_directory_for_testing\)");
+            R"(..\our_directory_for_testing\)");
     auto retrievedFolder = organizer.retrieve();
     auto path = retrievedFolder[0].path;
     auto name = retrievedFolder[0].name;
     auto size = retrievedFolder[0].size;
     auto dateCreated = retrievedFolder[0].dateCreated;
     auto dateModified = retrievedFolder[0].dateModified;
-    REQUIRE( retrievedFolder[0].path == R"(C:\Users\AlexL\Documents\UAF Classes\Software Construction\cs372-ufo\our_directory_for_testing\)");
-
+    REQUIRE( retrievedFolder[0].path.rfind(R"(\our_directory_for_testing\aura2_blue.png)") != string::npos );
 }
